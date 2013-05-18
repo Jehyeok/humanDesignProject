24af02c6385ed5b8f2ebab374bf69982e48eedd8
Jehyeok <jehyeok@Jehyeoks-MacBook-Pro.local>
2013-05-18 18:30:54 +0900

    second commit

---
+++ humanDesignProject/humanDesignProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  humanDesignProject
+//
+//  Created by Jehyeok on 5/18/13.
+//  Copyright (c) 2013 NHN Next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
